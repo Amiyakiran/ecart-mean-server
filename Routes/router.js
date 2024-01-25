69bdb3b91e99abf236584db5413501387c955610
amiyakiran <amiyakiran@gmail.com>
2024-01-25 07:17:44 +0530

    register and login

--- Routes/router.js
+++ Routes/router.js
@@ -2,12 +2,17 @@ const express = require('express')
 
 const productController = require('../Controllers/productController')
 
+const userController = require('../Controllers/userController')
+
 const router = new express.Router()
 
 //get all products
 router.get('/products/all',productController.getAllProductsController)
 
+//register
+router.post('/register',userController.registerController)
 
-
+//login
+router.post('/login',userController.loginController)
 
 module.exports = router
